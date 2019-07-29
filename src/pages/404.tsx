dfdd1822f78049745df08de001b8334c4c9fffaf
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-07-29 23:02:45 +0900

    Add title to 404 page (#42)
    * Add title to 404 page
    
    * Add missing /

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,14 @@
 import React, { FC, ReactElement } from 'react'
+import { Helmet } from 'react-helmet'
 import Layout from '../components/layout'
 
 const NotFoundPage: FC = (): ReactElement => {
   return (
     <Layout>
+      <Helmet defer={false}>
+        <title>404 Not Found</title>
+      </Helmet>
+
       <p>404</p>
     </Layout>
   )
